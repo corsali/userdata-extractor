bc3ae72125ea3315b56b6434ac70a5b56a1d91cb
Kahtaf Alam <kahtaf@gmail.com>
2022-08-03 17:44:54 -0400

    Add useWebWorkers option

--- src/utils/loadZipFile.ts
+++ src/utils/loadZipFile.ts
@@ -1,8 +1,11 @@
 import { ZipFile } from "./zipFile.js";
 
-const loadZipFile = async (file: File): Promise<ZipFile> => {
+const loadZipFile = async (
+  file: File,
+  useWebWorkers = true
+): Promise<ZipFile> => {
   const zipFile = new ZipFile(file);
-  await zipFile.importFileSystem({ filenameEncoding: "utf-8" });
+  await zipFile.importFileSystem({ filenameEncoding: "utf-8", useWebWorkers });
   return zipFile;
 };
 
