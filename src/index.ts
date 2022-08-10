4ec71723184114ce40090b5c1a298239514597f9
Joseph Csoti <joseph@vana.org>
2022-08-10 12:14:07 -0400

    export additional types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Exported types
+ */
+import type { Database, QueryResult } from "./database/database.js";
+import type { SQLiteDatabase } from "./database/sqliteDatabase.js";
+/**
+ * Exported functions
+ */
 import { zipToSQLiteInstance } from "./services/zipToSQLite.js";
 import { validateEmailIntegrationZip } from "./services/zipValidator.js";
 import { extractTablesFromSqlQuery } from "./utils/extractTablesFromSqlQuery.js";
@@ -11,3 +19,5 @@ export {
   validateEmailIntegrationZip,
   zipToSQLiteInstance,
 };
+
+export type { Database, QueryResult, SQLiteDatabase };
