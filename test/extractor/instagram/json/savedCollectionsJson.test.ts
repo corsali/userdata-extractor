5bed2341206d38a83ac94e89dbb5c605c5ea095b
Roman Meszaros <roman.meszaros@toptal.com>
2022-09-07 12:55:08 +0200

    FIX JSON Extractor - SavedCollectionsJson

--- test/extractor/instagram/json/savedCollectionsJson.test.ts
+++ test/extractor/instagram/json/savedCollectionsJson.test.ts
@@ -17,21 +17,31 @@ describe("Saved Collections (JSON)", () => {
     savedCollectionsJson.process();
 
     const { rows } = savedCollectionsJson.table;
-
-    expect(rows.length).toEqual(7);
+console.log(rows)
+    expect(rows.length).toEqual(6);
     expect(rows[0].name).toEqual(new TextTableValue("Spangly heroes"));
     expect(rows[0].created_date).toEqual(new DateTableValue(1536379872));
     expect(rows[0].updated_date).toEqual(new DateTableValue(1536379881));
     expect(rows[0].shared_url).toEqual(new UrlTableValue(""));
     expect(rows[0].shared_username).toEqual(new TextTableValue(""));
     expect(rows[0].shared_date).toEqual(new DateTableValue(""));
-    expect(rows[6].name).toEqual(new TextTableValue(""));
-    expect(rows[6].created_date).toEqual(new DateTableValue(""));
-    expect(rows[6].updated_date).toEqual(new DateTableValue(""));
-    expect(rows[6].shared_url).toEqual(
-      new UrlTableValue("https://www.instagram.com/p/BT0uvlLAjfD/")
+    expect(rows[1].name).toEqual(new TextTableValue("Cool"));
+    expect(rows[1].created_date).toEqual(new DateTableValue(1494532432));
+    expect(rows[1].updated_date).toEqual(new DateTableValue(1573870677));
+    expect(rows[1].shared_url).toEqual(
+      new UrlTableValue("https://www.instagram.com/p/CYp1KUYhVFk/")
     );
-    expect(rows[6].shared_username).toEqual(new TextTableValue("lilithcreek"));
-    expect(rows[6].shared_date).toEqual(new DateTableValue(1494532432));
+    expect(rows[1].shared_username).toEqual(
+      new TextTableValue("workartlife_by_aa")
+    );
+    expect(rows[1].shared_date).toEqual(new DateTableValue(1648621994));
+    expect(rows[2].name).toEqual(new TextTableValue("Cool"));
+    expect(rows[2].created_date).toEqual(new DateTableValue(1494532432));
+    expect(rows[2].updated_date).toEqual(new DateTableValue(1573870677));
+    expect(rows[2].shared_url).toEqual(
+      new UrlTableValue("https://www.instagram.com/p/B4MQBnZgCCk/")
+    );
+    expect(rows[2].shared_username).toEqual(new TextTableValue("bear.ltd"));
+    expect(rows[2].shared_date).toEqual(new DateTableValue(1573870723));
   });
 });
